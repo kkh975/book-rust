f0f25739af7846136e4b0f4880487a5898b02a87
kkh975 <kkh975@naver.com>
2023-08-13 13:20:54 +0900

    ch05 완료

---
+++ ch05_07_1_web_hello/src/main.rs
@@ -0,0 +1,21 @@
+use actix_web::{web, App, HttpServer, HttpRequest};
+
+const SERVER_ADDR: &str = "127.0.0.1:8888";
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("[SERVER] http://{}/", SERVER_ADDR);
+    HttpServer::new(|| {
+        // 라우팅 지정
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind(SERVER_ADDR)?
+    .run()
+    .await
+}
+
+async fn index(req: HttpRequest) -> &'static str {
+    println!("request: {:?}", req);
+    "Hello, World!"
+}
